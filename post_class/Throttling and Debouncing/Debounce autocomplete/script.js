b63ea965a514a9b20bf8228932daf1cb07ce92eb
pravesh meena <112716122+pravesh2892@users.noreply.github.com>
2023-06-16 10:33:34 +0530

    assign

---
+++ post_class/Throttling and Debouncing/Debounce autocomplete/script.js
@@ -0,0 +1,31 @@
+var nameList = ["Account", "Accuracy", "Brave", "Bold", "Bachelor", "Absolute", "Absorb", "Academy", "Accent", "Apple", "Amusement", "Ambulance", "Apricot", "Avocado"];
+
+const searchHandler = () => {
+  let suggestionsListContainer = document.getElementById("searchOutput");
+  let searchTerm = searchInput.value.toLowerCase();
+
+  let filteredSearch = nameList.filter((name) => name.toLowerCase().startsWith(searchTerm));
+  suggestionsListContainer.innerHTML = "";
+  if (filteredSearch.length === nameList.length)
+    return;
+  filteredSearch.forEach(function(suggestion) {
+    let li = document.createElement("li");
+    li.textContent = suggestion;
+    suggestionsListContainer.appendChild(li);
+  });
+};
+
+const debounce = (callbackFn, wait) => {
+  let timeoutId;
+
+  return function() {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      callbackFn.apply(this, arguments);
+    }, wait);
+  };
+};
+
+const debouncedSearchHandler = debounce(searchHandler, 500);
+document.getElementById('searchInput').addEventListener('keyup', debouncedSearchHandler);
