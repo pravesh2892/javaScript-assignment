a1badca959d74a3acd0a48b4e64465a3db6ffde7
pravesh meena <112716122+pravesh2892@users.noreply.github.com>
2023-06-19 08:42:45 +0530

    assignment

--- practice/script.js
+++ practice/script.js
@@ -1,18 +1,18 @@
-function handleSubmit() {
-    var username = document.getElementById('name').value;
-    var email = document.getElementById("mail").value;
-    var contact = document.getElementById("contact").value;
+// function handleSubmit() {
+//     var username = document.getElementById('name').value;
+//     var email = document.getElementById("mail").value;
+//     var contact = document.getElementById("contact").value;
     
-    var message = "Username: " + username + "\nEmail: " + email + "\nMobile no.: " + contact;
+//     var message = "Username: " + username + "\nEmail: " + email + "\nMobile no.: " + contact;
     
-    alert(message);
-    console.log(message);
-  }
+//     alert(message);
+//     console.log(message);
+//   }
   
-  document.getElementById('subBtn').addEventListener("submit", function(event) {
-    event.preventDefault();
-    handleSubmit();
-  });
+//   document.getElementById('subBtn').addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     handleSubmit();
+//   });
 
 // var submit = document.getElementById("sub");
 // submit.addEventListener('click', function(event){
@@ -22,3 +22,48 @@ function handleSubmit() {
 
 //     alert("Name: " + Name + "Email: " + email);
 // });
+
+// ----------callback function---------------
+
+// function add(name){
+//     //return a+b;
+//     console.log("Meena");
+// }
+
+// function logging(callback){
+//     console.log("hello pravesh");
+//     callback(name);
+// }
+
+// let result = logging();
+// console.log(result);
+
+
+
+
+//==============================================================================
+
+
+// Function.prototype.myCall = function(context, args) {
+//     const func = this;
+//     const copyContext = { ...context };
+//     copyContext.fnName = func;
+//     return copyContext.fnName.apply(copyContext, args);
+//   };
+  
+//   obj2.foo.myCall(obj1, 10, 11, 12, 13);
+
+
+  function sum(a){
+    return function(b){
+        return a+b;
+    }
+  }
+
+  sum(1)(2)
+
+
+
+
+
+
